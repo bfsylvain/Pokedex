b23cec19f088a297b7c15cf4e810613022f30d45
bfsylvain <sylvainbonnaure@hotmail.com>
2023-10-08 12:18:49 +0200

    exercice

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import PokemonCard from './components/PokemonCard'
 import './App.css'
 
@@ -13,4 +10,4 @@ function App() {
   );
 }
 
-export default App
+export default App;

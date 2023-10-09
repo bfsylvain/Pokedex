704c39a28a7edcedad67d694225c92745521b08f
bfsylvain <sylvainbonnaure@hotmail.com>
2023-10-09 22:35:08 +0200

    modifications props.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -4,15 +4,6 @@ import PropTypes from "prop-types";
 function PokemonCard({pokemon}) {
     console.log({pokemon})
 
-    PokemonCard.PropTypes = {
-        pokemon: PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            imgSrc: PropTypes.string.isRequired
-        })
-    }
-
-
-    // const Pokemon = PokemonList[0]
     const IsImage = (element) => {
     return element.imgSrc 
     ? <img src={element.imgSrc} alt={element.name}/> 
@@ -25,6 +16,13 @@ function PokemonCard({pokemon}) {
             <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
+}
+
+PokemonCard.PropTypes = {
+    pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string
+    })
 }
 
 
